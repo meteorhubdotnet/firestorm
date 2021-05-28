496275d793cdb216022a6a982641844b317d190c
Yacine Merzouk <yacine@merzouk.ca>
2021-05-27 19:08:36 -1000

    Updated router

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'meteorhubdotnet:firestorm',
     summary: 'Rapid Development Framework For Meteor With Blaze And Iron Router',
-    version: '1.0.5',
+    version: '1.0.6',
     git: 'https://github.com/meteorhubdotnet/firestorm'
 });
 
@@ -13,11 +13,11 @@ Package.onUse(function (api) {
     api.use('check');
     // api.use('tmeasday:check-npm-versions');
     // api.use('meteorhubdotnet:bureaucrat');
-    api.use('meteorhubdotnet:firestorm-iron-router@1.0.4');
+    api.use('meteorhubdotnet:firestorm-iron-router@1.0.5');
     api.use('meteorhubdotnet:firestorm-useraccounts-core@1.0.4');
     api.use('meteorhubdotnet:firestorm-iron-useraccounts@1.0.4');
     api.use('meteorhubdotnet:firestorm-useraccounts-unstyled@1.0.4');
-    api.use('aldeed:collection2@3.4.0', { weak: true });
+    api.use('aldeed:collection2@3.3.0');
     // api.use('meteorhubdotnet:collection-helpers');
 
 });
