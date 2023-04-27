422d8b409195e3fc3c4e8bbc495828d03c9f1028
Yacine Merzouk <yacine@merzouk.ca>
2023-04-26 20:40:56 -0400

    1.0.15

--- package.js
+++ package.js
@@ -1,26 +1,21 @@
 Package.describe({
     name: 'meteorhubdotnet:firestorm',
     summary: 'Rapid Development Framework For Meteor With Blaze And Iron Router',
-    version: '1.0.14',
+    version: '1.0.15',
     git: 'https://github.com/meteorhubdotnet/firestorm'
 });
 
 Package.onUse(function (api) {
-
-    api.versionsFrom('METEOR@2.7.3');
+    api.versionsFrom('METEOR@2.11.0');
     api.use('accounts-password');
     api.use('less@4.0.0');
     api.use('check');
-    // api.use('tmeasday:check-npm-versions');
-    // api.use('meteorhubdotnet:bureaucrat');
-    api.use('meteorhubdotnet:firestorm-iron-router@1.0.10');
-    api.use('meteorhubdotnet:firestorm-helpers@1.0.10');
-    api.use('meteorhubdotnet:firestorm-useraccounts-core@1.0.10');
-    api.use('meteorhubdotnet:firestorm-iron-useraccounts@1.0.10');
-    api.use('meteorhubdotnet:firestorm-useraccounts-unstyled@1.0.10');
+    api.use('meteorhubdotnet:firestorm-iron-router@1.0.15');
+    api.use('meteorhubdotnet:firestorm-helpers@1.0.15');
+    api.use('meteorhubdotnet:firestorm-useraccounts-core@1.0.15');
+    api.use('meteorhubdotnet:firestorm-iron-useraccounts@1.0.15');
+    api.use('meteorhubdotnet:firestorm-useraccounts-unstyled@1.0.15');
     api.use('aldeed:collection2@3.5.0');
-    // api.use('meteorhubdotnet:collection-helpers');
-
 });
 
-Npm.depends({ 'simpl-schema':'1.12.3' });
+Npm.depends({ 'simpl-schema':'3.4.1' });
